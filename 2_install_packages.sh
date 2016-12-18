76216f449602baf9f5395fbb4ac926c45b3c02ed
atm-ph <phf@atm-consulting.fr>
2016-12-18 15:05:16 +0100

    Update add php xml package to handle utf8 function

--- 2_install_packages.sh
+++ 2_install_packages.sh
@@ -1,6 +1,6 @@
 apt-get update
 
 # Install Packages standard
-apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd
+apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd php7.0-xml
 
 service apache2 restart

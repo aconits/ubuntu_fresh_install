5f9754438ffc77069bf9bc6ac7b8f45718d0d400
atm-ph <phf@atm-consulting.fr>
2017-03-13 18:58:43 +0100

    Update add xdebug package

--- 2_install_packages.sh
+++ 2_install_packages.sh
@@ -1,6 +1,6 @@
 apt-get update
 
 # Install Packages standard
-apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd php7.0-xml
+apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd php7.0-xml php-xdebug
 
 service apache2 restart

d7cb350799a2eb8a99945578806eb0a38a6724d5
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-03-28 20:40:12 +0200

    add rust insall for ripgrep

--- 2_install_packages.sh
+++ 2_install_packages.sh
@@ -4,3 +4,6 @@ apt-get update
 apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd php7.0-xml php-xdebug
 
 service apache2 restart
+
+curl -sSf https://static.rust-lang.org/rustup.sh | sh
+cargo install ripgrep

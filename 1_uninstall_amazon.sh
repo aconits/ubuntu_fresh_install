c9f82e9a916f5519eec4c117c650c7b436498d22
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-07-17 09:14:53 +0200

    Update 1_uninstall_amazon.sh

--- 1_uninstall_amazon.sh
+++ 1_uninstall_amazon.sh
@@ -3,3 +3,4 @@
 
 # Uninstall ubuntu amazon
 dpkg --purge unity-webapps-common unity-webapps-qml unity-webapps-service webapp-container webbrowser-app libunity-webapps0
+apt-get install gnome-control-center 

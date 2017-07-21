13497465b0f68bbfd9f7feed441b43042146bf35
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-07-21 14:56:15 +0200

    Update 0_run.sh

--- 0_run.sh
+++ 0_run.sh
@@ -1,7 +1,7 @@
 sh ./1_uninstall_amazon.sh
-sh ./2_install_packages.sh
-sh ./3_install_adminer.sh
-sh ./4_remove_packages.sh
+sh ./2_remove_packages.sh
+sh ./3_install_packages.sh
+sh ./4_install_adminer.sh
 sh ./5_others.sh
 sh ./6_clean.sh
 

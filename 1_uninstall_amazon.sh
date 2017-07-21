3ce66a60f6b1bca45dd7d22805e481252dba8061
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-07-21 16:21:15 +0200

    Update 1_uninstall_amazon.sh

--- 1_uninstall_amazon.sh
+++ 1_uninstall_amazon.sh
@@ -2,5 +2,5 @@
 # dpkg -r unity-tweak-tool
 
 # Uninstall ubuntu amazon
-dpkg --purge unity-webapps-common unity-webapps-qml unity-webapps-service webapp-container webbrowser-app libunity-webapps0
-apt-get install unity-control-center
+# dpkg --purge unity-webapps-common unity-webapps-qml unity-webapps-service webapp-container webbrowser-app libunity-webapps0
+# apt-get install unity-control-center

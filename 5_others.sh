3f0f6075010787e17dfe0334292c9ae01ed7ccf7
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-07-21 15:00:55 +0200

    Update 5_others.sh

--- 5_others.sh
+++ 5_others.sh
@@ -2,3 +2,6 @@ echo "vm.swappiness=5" | sudo tee -a /etc/sysctl.conf
 
 # Accélérer le démarrage de certains programmes, dont LibreOffice
 echo 127.0.0.1 $HOSTNAME localhost $HOSTNAME"(.none)" | sudo tee -a /etc/hosts
+
+# Update de l'éditeur par défaut (choix à faire)
+update-alternatives --config editor

88d700d3f17825cd3e7f1d5cf01ae4ab3e1509e7
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2017-07-25 10:47:46 +0200

    Update 3_install_packages.sh

--- 3_install_packages.sh
+++ 3_install_packages.sh
@@ -1,7 +1,7 @@
 apt-get update
 
 # Install Packages standard
-apt-get install apache2 apache2-utils mysql-server php7.0 php7.0-mysql libapache2-mod-php7.0 mysql-server git vim terminator php7.0-curl php7.0-gd php7.0-xml php-xdebug php7.0-zip
+apt-get install apache2 apache2-utils mysql-server mysql-client php7.0 php7.0-mysql libapache2-mod-php7.0 git vim terminator php7.0-curl php7.0-gd php7.0-xml php-xdebug php7.0-zip
 
 service apache2 restart
 

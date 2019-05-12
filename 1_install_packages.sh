9538cef571b99a23384265cced81f4fb65d802ce
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2019-05-12 19:17:37 +0200

    Update 1_install_packages.sh

--- 1_install_packages.sh
+++ 1_install_packages.sh
@@ -1,7 +1,7 @@
 apt update
 
 # Install Packages
-apt install net-tools gnome-tweak-tool iotop htop ntp git locate bash-completion vim bzip2 zip gzip terminator vlc shutter chromium-browser meld virtualbox filezilla
+apt install net-tools gnome-tweak-tool iotop htop ntp git locate bash-completion vim bzip2 zip gzip terminator vlc chromium-browser meld virtualbox filezilla
 apt install apache2 libapache2-mod-php apache2-utils 
 apt install mariadb-client mariadb-server 
 apt install php php-mysql php-curl php-bz2 php-gd php-zip php-xml php-xdebug
@@ -15,3 +15,24 @@ service apache2 restart
 
 # Menu principal : permet de gérer les lanceurs et leurs icones
 #apt-get install alacarte
+
+
+#apt install shutter
+## Ré-activer l'option "éditer" de shutter
+#mkdir /tmp/libgoo-canvas-perl && cd /tmp/libgoo-canvas-perl
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libg/libgoo-canvas-perl/libgoo-canvas-perl_0.06-2ubuntu3_amd64.deb
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libe/libextutils-depends-perl/libextutils-depends-perl_0.405-1_all.deb
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/libe/libextutils-pkgconfig-perl/libextutils-pkgconfig-perl_1.15-1_all.deb
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/g/goocanvas/libgoocanvas3_1.0.0-1_amd64.deb
+#wget http://archive.ubuntu.com/ubuntu/pool/universe/g/goocanvas/libgoocanvas-common_1.0.0-1_all.deb
+#sudo dpkg -i *.deb
+#sudo apt install -f
+#cd /tmp
+#rm -rf /tmp/libgoo-canvas-perl
+
+# alternative à shutter
+cd /tmp
+wget https://github.com/DamirPorobic/ksnip/releases/download/v1.5.0/ksnip-1.5.0.deb
+sudo dpkg -i ksnip-1.5.0.deb
+sudo apt install -f
+rm ksnip-1.5.0.deb

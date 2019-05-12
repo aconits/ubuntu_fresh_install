48f9345fd1e3aeb2ff11b3626269e2ec70e94a0f
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2019-05-12 15:14:54 +0200

    Update and rename 3_install_packages.sh to 1_install_packages.sh

---
+++ 1_install_packages.sh
@@ -0,0 +1,17 @@
+apt update
+
+# Install Packages
+apt install net-tools iotop htop ntp git locate bash-completion vim bzip2 zip gzip terminator vlc shutter chromium-browser meld virtualbox filezilla
+apt install apache2 libapache2-mod-php apache2-utils 
+apt install mariadb-client mariadb-server 
+apt install php php-mysql php-curl php-bz2 php-gd php-zip php-xml php-xdebug
+apt install phpmyadmin
+apt install openssh-server fail2ban
+
+service apache2 restart
+
+#curl -sSf https://static.rust-lang.org/rustup.sh | sh
+#cargo install ripgrep
+
+# Menu principal : permet de gérer les lanceurs et leurs icones
+#apt-get install alacarte

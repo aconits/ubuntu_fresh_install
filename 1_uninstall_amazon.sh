4f538fb0c01119a94a05c8194fbd09b864185c8f
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2019-05-12 13:03:04 +0200

    Delete 1_uninstall_amazon.sh

--- 1_uninstall_amazon.sh
+++
@@ -1,6 +0,0 @@
-# "unity-tweak-tool" doit être désintallé avant
-# dpkg -r unity-tweak-tool
-
-# Uninstall ubuntu amazon
-# dpkg --purge unity-webapps-common unity-webapps-qml unity-webapps-service webapp-container webbrowser-app libunity-webapps0
-# apt-get install unity-control-center

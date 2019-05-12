2d7ebb2689bbe61da80805451cbc1fbe57a2beea
FAVRE Pierre-Henry <phf@atm-consulting.fr>
2019-05-12 15:22:49 +0200

    Delete 0_run.sh

--- 0_run.sh
+++
@@ -1,9 +0,0 @@
-#sh ./1_uninstall_amazon.sh
-#sh ./2_remove_packages.sh
-sh ./3_install_packages.sh
-sh ./4_install_adminer.sh
-sh ./5_others.sh
-sh ./6_clean.sh
-sh ./7_create_ssh_key.sh
-
-echo "NEED REBOOT :)"
